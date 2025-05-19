45bdfc1a45c1c5332aab3b5009072b59b55d5b83
kramalmr <akram3107@student.abudzar.sch.id>
2025-05-19 10:37:07 +0700

    install

---
+++ portfolio-almair/src/App.jsx
@@ -0,0 +1,13 @@
+function App () {
+  return (
+    <div className="w-full h-screen px-8 py-5">
+      <div id="navbar flex flex-col w-full text-3xl">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
+}
+export default App;
